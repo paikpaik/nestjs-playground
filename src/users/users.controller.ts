46a3b19ee82413d4a60578b699086a9ddf758021
paikpaik <woduf9311@gmail.com>
2023-08-08 14:54:36 +0900

    Add: 데이터 전송객체 dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,7 +24,9 @@ export class UsersController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    const { name, email } = createUserDto;
+
+    return `유저를 생성했습니다. 이름 : ${name}, 이메일: ${email}`;
   }
 
   @Get()
